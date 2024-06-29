de620761c2890157be908adffd05571947d773f9
Cat Games Inc <148311731+Cat-Games@users.noreply.github.com>
2024-06-29 20:29:36 +0100

    Update script.js

--- gooserunners/files/script.js
+++ gooserunners/files/script.js
@@ -6,9 +6,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (prefersDarkMode) {
         body.classList.add("dark-mode");
+    }else{
+        body.classList.add("light-mode");
     }
 
     darkModeToggle.addEventListener("click", function () {
         body.classList.toggle("dark-mode");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("light-mode-toggle");
+    const body = document.body;
+
+    const prefersLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
+
+    if (prefersLightMode) {
+        body.classList.add("light-mode");
+    }
+
+    darkModeToggle.addEventListener("click", function () {
+        body.classList.toggle("light-mode");
     });
 });
