bb92a6651c2836004a8e5700eac644ed74d89a40
Cat Games Inc <148311731+Cat-Games@users.noreply.github.com>
2024-07-30 00:56:29 +0100

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownButton = document.querySelector('.dropdown-button');
+    var dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdownButton.addEventListener('click', function() {
+        dropdownContent.classList.toggle('show');
+    });
+
+    window.addEventListener('click', function(event) {
+        if (!event.target.matches('.dropdown-button')) {
+            if (dropdownContent.classList.contains('show')) {
+                dropdownContent.classList.remove('show');
+            }
+        }
+    });
+});
 
